abb1348503a7b8fff0642d294b56e48e3cd84b9c
MadDeveloper <sergent.julien@icloud.com>
2018-02-28 20:22:21 +0100

    feat(sections): add personal experiences section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
-import { HttpClientModule, HttpClient } from '@angular/common/http'
-import { TranslateHttpLoader } from '@ngx-translate/http-loader'
-
+import { AboutComponent } from './about/about.component'
 import { AppComponent } from './app.component'
-import { LangService } from './services/lang.service';
-import { ProfileComponent } from './profile/profile.component';
-import { AboutComponent } from './about/about.component';
-import { ExperiencesComponent } from './experiences/experiences.component';
-import { StudiesComponent } from './studies/studies.component';
-import { SkillsComponent } from './skills/skills.component';
-import { ToolsComponent } from './tools/tools.component';
+import { BrowserModule } from '@angular/platform-browser'
+import { ExperiencesComponent } from './experiences/experiences.component'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { LangService } from './services/lang.service'
 import { LanguagesComponent } from './languages/languages.component'
+import { NgModule } from '@angular/core'
+import { PersonalExperiencesComponent } from './personal-experiences/personal-experiences.component'
+import { ProfileComponent } from './profile/profile.component'
+import { SkillsComponent } from './skills/skills.component'
+import { StudiesComponent } from './studies/studies.component'
+import { ToolsComponent } from './tools/tools.component'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
+
 
 export function createTranslateLoader(http: HttpClient) {
     return new TranslateHttpLoader(http, './assets/translations/', '.json')
@@ -24,6 +25,7 @@ export function createTranslateLoader(http: HttpClient) {
     ProfileComponent,
     AboutComponent,
     ExperiencesComponent,
+    PersonalExperiencesComponent,
     StudiesComponent,
     SkillsComponent,
     ToolsComponent,
---
+++ src/app/personal-experiences/personal-experiences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-personal-experiences',
+  templateUrl: './personal-experiences.component.html',
+  styleUrls: ['./personal-experiences.component.scss']
+})
+export class PersonalExperiencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
